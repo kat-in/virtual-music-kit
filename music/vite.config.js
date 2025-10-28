8fbcb0820ee79fda49ca93cba66d81d0cdcd9621
kat-in <newkatelife@gmail.com>
2025-10-28 11:21:58 +0300

    config: update vite for GH Pages

--- music/vite.config.js
+++ music/vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: '/virtual-music-kit/music',  
+  base: '/virtual-music-kit/',  
   build: {
     sourcemap: true,
     minify: false,
